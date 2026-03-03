65428d90713e5839a24728631681883f08fec528
efmomoh <efmomoh25@gmail.com>
2026-03-04 06:39:47 +0800

    Modifies file

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,6 +1,6 @@
 // foooter js
 
-const currentYear = document.querySelector('#currentYear');
-currentYear.textContent = `@${new Date().getFullYear()}🌲Taurus ProMax Global Network 🌐`;
+const currentYear = document.querySelector('#currentyear');
+currentYear.innerHTML = `@${new Date().getFullYear()}🌲Taurus ProMax Global Network - <em>LIBERIA</em> 🌐`;
 const lastDateModified = document.querySelector('#lastModified');
 lastDateModified.textContent = `LastModified: ${document.lastModified}`;
