2f61c0f115ca017f800091df18a74d9ebb864875
efmomoh <efmomoh25@gmail.com>
2026-03-04 02:33:31 +0800

    Adds week01 folder

---
+++ week01/scripts/app.js
@@ -0,0 +1,5 @@
+const currentYear = document.querySelector('#year').textContent = `Copyright @ ${new Date().getFullYear()} Taurus ProMax`;;
+const lastModifiedDate = document.querySelector('#lastModified').textContent = `LastModified: ${document.lastModified}`;
+
+
+
