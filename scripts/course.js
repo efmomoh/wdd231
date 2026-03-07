338738ebe24b0f07c4d3349f056862674f07831f
efmomoh <efmomoh25@gmail.com>
2026-03-08 07:13:32 +0800

    Add file

---
+++ scripts/course.js
@@ -0,0 +1,136 @@
+// COURSE JS
+
+// object array of courses 
+const courses = [
+    {
+        subject: 'CSE',
+        number: 110,
+        title: 'Introduction to Programming',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.',
+        technology: [
+            'Python'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 130,
+        title: 'Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
+        technology: [
+            'HTML',
+            'CSS'
+        ],
+        completed: true
+    },
+    {
+        subject: 'CSE',
+        number: 111,
+        title: 'Programming with Functions',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
+        technology: [
+            'Python'
+        ],
+        completed: true
+    },
+    {
+        subject: 'CSE',
+        number: 210,
+        title: 'Programming with Classes',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.',
+        technology: [
+            'C#'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 131,
+        title: 'Dynamic Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 231,
+        title: 'Frontend Web Development I',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: false
+    }
+]
+
+
+const cardContainer = document.querySelector('.card-list');
+const DisplayCredits = document.querySelector('#credits');
+
+const buttonAll = document.querySelector('#all');
+const buttonWdd = document.querySelector('#wdd');
+const buttonCse = document.querySelector('#cse');
+
+function displayCourses(courseList) {
+    cardContainer.innerHTML = courseList.map(course => `
+        <section class="course-card ${course.completed ? 'completed' : 'not-completed'}">
+
+        <h5>
+        ${course.subject} ${course.number}
+        ${course.completed ? "✅" : "❌"}
+        </h5>
+
+        <p>${course.title}</p>
+        <p>Status: ${course.completed ? "Completed" : "Not Completed"}</p>
+
+        </section>
+        `).join("");
+    
+    // add the total credits completed
+    const creditsCompleted = courseList.filter(course => course.completed).reduce((total, course) => total + course.credits, 0)
+
+    // total credits not completed
+    const creditsNotCompleted = courseList.filter(course => !course.completed).reduce((total, course) => total + course.credits, 0)
+
+    // display the credits
+    DisplayCredits.textContent = `Completed Status ${creditsCompleted} | Remaining Credits: ${creditsNotCompleted}`
+}
+
+// add event for the buttons
+buttonAll.addEventListener('click', () => {
+    displayCourses(courses);
+});
+
+buttonWdd.addEventListener('click', () => {
+    const wddCourses = courses.filter(course => course.subject === "WDD");
+    displayCourses(wddCourses);
+});
+
+buttonCse.addEventListener('click', () => {
+    const cseCourses = courses.filter(course => course.subject === "CSE");
+    displayCourses(cseCourses);
+})
+
+// call the courses, load, and display
+displayCourses(courses);
+
+
+
