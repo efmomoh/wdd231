638ea16d72fa9deaf8b4fc6458fa97ecc53f79a5
efmomoh <efmomoh25@gmail.com>
2026-03-24 12:01:14 +0800

    Modifies API unit to use 'metric'

--- week03/scripts/api-exercise.js
+++ week03/scripts/api-exercise.js
@@ -4,7 +4,7 @@ const weatherIcon = document.getElementById("weather-icon");
 const FigCaptionDescription = document.querySelector("figcaption");
 
 // API KEY: 49.7500468819332, 6.637010217773472
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=metrics&appid=625e320da1dff73739d188728d972a31";
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=metric&appid=625e320da1dff73739d188728d972a31";
 
 async function apiFetch() {
     try {
@@ -25,7 +25,7 @@ async function apiFetch() {
 apiFetch();
 
 function displayResults(data) {
-    currentTemperature.textContent = `${data.main.temp}°F`;
+    currentTemperature.textContent = `${data.main.temp}°C`;
     const iconCode = data.weather[0].icon;
     weatherIcon.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
     weatherIcon.alt = data.weather[0].description;
